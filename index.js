f69cc53b7f4d0ea8b80d569acce47beb6a940bd1
manassuthar2 <sutharmanas25@gmail.com>
2026-02-03 16:03:48 +0530

    manas

---
+++ index.js
@@ -0,0 +1,8 @@
+function showSkills() {
+    document.getElementById("skills").innerHTML =
+        "• HTML<br>" +
+        "• CSS<br>" +
+        "• JavaScript<br>" +
+        "• Java Programming<br>" +
+        "• Data Structures";
+}
